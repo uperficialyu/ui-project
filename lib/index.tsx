bc1d884e321804de6e635619e674d48c35bc6ebf
Emily <947201766@qq.com>
2020-02-09 22:03:36 +0800

    安装react

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,4 +1,11 @@
-console.log('hello world4')
-const div = document.createElement('div');
-div.innerText = 'div2';
-document.body.append(div);
+import React from 'react';
+import ReactDom from 'react-dom';
+
+
+// console.log('hello world4')
+// const div = document.createElement('div');
+// div.innerText = 'div2';
+// document.body.append(div);
+
+console.log(React);
+console.log(ReactDom);
