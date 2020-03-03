785070d16c195b7b280a9b80925d0abe0614c147
Emily <947201766@qq.com>
2020-03-03 21:27:27 +0800

    aside bug

--- lib/helpers/classes.tsx
+++ lib/helpers/classes.tsx
@@ -19,6 +19,18 @@ function scopedClassMaker(prefix: string) {
   }
 }
 
+// const scopedClassMaker = (prefix: string) =>
+//   (name: string | ClassToggles, options?: Options) =>
+//   Object
+//     .entries(name instanceof Object ? name : {[name]: name})
+//     .filter(kv => kv[1] !== false)
+//     .map(kv => kv[0])
+//     .map(name => [prefix, name]
+//       .filter(Boolean)
+//       .join('-'))
+//     .concat(options && options.extra || [])
+//     .join(' ');
+
 export {
   scopedClassMaker
 };
