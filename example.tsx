8ab90bbee8f32d95f7edac5026e932c525b4a1fe
Emily <947201766@qq.com>
2020-03-14 19:48:18 +0800

    样式

--- example.tsx
+++ example.tsx
@@ -4,18 +4,23 @@ import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import IconExample from './lib/icon/Icon.example';
 import DialogExample from './lib/dialog/Dialog.example';
 import LayoutExample from './lib/layout/Layout.example';
+import { Layout, Aside, Header, Content, Footer } from './lib/layout/Layout';
+import './example.scss';
+
+const logo = require('./logo.png');
+
 
 ReactDOM.render(
   <Router>
-    <div>
-      <header>
+    <Layout className="site-page">
+      <Header>
         <div className="logo">
-          Emily-ui
+          <img src={logo.default} width="48" height="48" alt=""/>
+          <span> Emily-ui </span>
         </div>
-
-      </header>
-      <div>
-        <aside>
+      </Header>
+      <Layout>
+        <Aside>
           <h2>组件</h2>
           <ul>
             <li><Link to="/icon">图标</Link></li>
@@ -23,13 +28,16 @@ ReactDOM.render(
             <li><Link to="/dialog">对话框</Link></li>
             <li><Link to="/layout">布局</Link></li>
           </ul>
-        </aside>
-        <main>
+        </Aside>
+        <Content>
           <Route path="/icon" component={IconExample} />
           <Route path="/dialog" component={DialogExample} />
           <Route path="/layout" component={LayoutExample} />
-        </main>
-      </div>
-    </div>
+        </Content>
+      </Layout>
+      <Footer className="site-footer">
+        &copy; 方应杭
+      </Footer>
+    </Layout>
   </Router>
   , document.querySelector('#root'));
--- lib/layout/Layout.tsx
+++ lib/layout/Layout.tsx
@@ -24,3 +24,8 @@ const Layout: React.FunctionComponent<Props> = (props) => {
 };
 
 export default Layout;
+export {Layout};
+export { default as Header } from './Header';
+export { default as Content } from './Content';
+export { default as Footer } from './Footer';
+export { default as Aside } from './Aside';
