08215f9d48dfc830176cbb0b4e7f35740b7f8d09
Emily <947201766@qq.com>
2020-04-13 22:34:39 +0800

    计算滚动条高度

--- lib/scroll/Scroll.tsx
+++ lib/scroll/Scroll.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { HTMLAttributes } from 'react';
+import { HTMLAttributes, UIEventHandler, useState, useEffect, useRef } from 'react';
 import scrollbarWidth from './scrollbar-width';
 
 interface Props extends HTMLAttributes<HTMLDivElement> {
@@ -8,13 +8,53 @@ interface Props extends HTMLAttributes<HTMLDivElement> {
 
 const Scroll: React.FunctionComponent<Props> = (props) => {
   const { children, ...rest } = props;
+  const [barHeight, setBarHeight] = useState(0);
+
+  const onScroll: UIEventHandler = (e) => {
+    // const scrollHeight = e.currentTarget.scrollHeight;
+    // const viewHeight = e.currentTarget.getBoundingClientRect().height;
+    // setBarHeight(viewHeight * viewHeight / scrollHeight)
+
+    console.log(e)
+    // setBarVisible(true);
+    // const { current } = containerRef;
+    // const scrollHeight = current!.scrollHeight;
+    // const viewHeight = current!.getBoundingClientRect().height;
+    // const scrollTop = current!.scrollTop;
+    // setBarTop(scrollTop * viewHeight / scrollHeight);
+    // if (timerIdRef.current !== null) {
+    //   window.clearTimeout(timerIdRef.current!);
+    // }
+    // timerIdRef.current = window.setTimeout(() => {
+    //   setBarVisible(false);
+    // }, 300);
+  };
+
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => { // mounted 的时候算滚动条高度
+    const scrollHeight = containerRef.current!.scrollHeight;
+    const viewHeight = containerRef.current!.getBoundingClientRect().height;
+    setBarHeight(viewHeight * viewHeight / scrollHeight);
+  }, []);
+
   return (
     <div
       className="emily-scroll"
       {...rest}
     >
-      <div className="fui-scroll-inner" style={{right: -scrollbarWidth()}}>
+      <div className="emily-scroll-inner" style={{right: -scrollbarWidth()}}
+        onScroll={onScroll}
+        ref={containerRef}
+      >
       {children}
+      </div>
+      <div className="emily-scroll-track">
+        <div className="emily-scroll-bar" 
+          style={{ height: barHeight }}
+          // style={{height: barHeight, transform: `translateY(${barTop}px)`}}
+          // onMouseDown={onMouseDownBar}
+        />
       </div>
     </div>
   )
