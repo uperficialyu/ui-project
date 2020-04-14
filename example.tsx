05c1a99855c7490f68c0e456d8f586adfd0fb3e8
Emily <947201766@qq.com>
2020-04-14 23:06:25 +0800

    拖动滚动条

--- example.tsx
+++ example.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { HashRouter as Router, Route, NavLink } from 'react-router-dom';
 // import IconExample from './lib/icon/Icon.example';
 import IconDemo from './lib/icon/Icon.demo';
-
+import ButtonExample from './lib/button/Button.example';
 import DialogExample from './lib/dialog/Dialog.example';
 import LayoutExample from './lib/layout/Layout.example';
 import { Layout, Aside, Header, Content, Footer } from './lib/layout/Layout';
@@ -48,6 +48,7 @@ ReactDOM.render(
         </Aside>
         <Content className="site-main">
           <Route path="/icon" component={IconDemo} />
+          <Route path="/button" component={ButtonExample} />
           <Route path="/dialog" component={DialogExample} />
           <Route path="/layout" component={LayoutExample} />
           <Route path="/form" component={FormExample} />
--- lib/scroll/Scroll.tsx
+++ lib/scroll/Scroll.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { HTMLAttributes, UIEventHandler, useState, useEffect, useRef } from 'react';
+import { HTMLAttributes, UIEventHandler, useState, useEffect, useRef, MouseEventHandler } from 'react';
 import scrollbarWidth from './scrollbar-width';
 
 interface Props extends HTMLAttributes<HTMLDivElement> {
@@ -9,14 +9,28 @@ interface Props extends HTMLAttributes<HTMLDivElement> {
 const Scroll: React.FunctionComponent<Props> = (props) => {
   const { children, ...rest } = props;
   const [barHeight, setBarHeight] = useState(0);
-  const [barTop, setBarTop] = useState(0);
+  const [barTop, _setBarTop] = useState(0);
+
+  const containerRef = useRef<HTMLDivElement>(null);
+  const draggingRef = useRef(false);
+  const firstYRef = useRef(0);
+  const firstBarTopRef = useRef(0);
+
+  const setBarTop = (number: number) =>{
+    if (number < 0) {
+      return;
+    }
+    const { current } = containerRef;
+    const scrollHeight = current!.scrollHeight;
+    const viewHeight = current!.getBoundingClientRect().height;
+    const maxBarTop = (scrollHeight - viewHeight) * viewHeight / scrollHeight;
+    if (number > maxBarTop) {
+      return;
+    }
+    _setBarTop(number);
+  };
 
   const onScroll: UIEventHandler = (e) => {
-    // const scrollHeight = e.currentTarget.scrollHeight;
-    // const viewHeight = e.currentTarget.getBoundingClientRect().height;
-    // setBarHeight(viewHeight * viewHeight / scrollHeight)
-
-    console.log(e)
     // setBarVisible(true);
     const { current } = containerRef;
     const scrollHeight = current!.scrollHeight;
@@ -31,12 +45,40 @@ const Scroll: React.FunctionComponent<Props> = (props) => {
     // }, 300);
   };
 
-  const containerRef = useRef<HTMLDivElement>(null);
-
   useEffect(() => { // mounted 的时候算滚动条高度
     const scrollHeight = containerRef.current!.scrollHeight;
     const viewHeight = containerRef.current!.getBoundingClientRect().height;
     setBarHeight(viewHeight * viewHeight / scrollHeight);
+  }, []);
+
+  const onMouseDownBar: MouseEventHandler = (e: any) => {
+    draggingRef.current = true;
+    firstYRef.current = e.clientY;
+    firstBarTopRef.current = barTop;
+  };
+
+  const onMouseMoveBar = (e: MouseEvent) => {
+    if (draggingRef.current) {
+      const delta = e.clientY - firstYRef.current;
+      const newBarTop = firstBarTopRef.current + delta;
+      setBarTop(newBarTop);
+      // const scrollHeight = containerRef.current!.scrollHeight;
+      // const viewHeight = containerRef.current!.getBoundingClientRect().height;
+      // containerRef.current!.scrollTop = newBarTop * scrollHeight / viewHeight;
+    }
+  };
+
+  const onMouseUpBar = () =>{
+    draggingRef.current = false;
+  }
+
+  useEffect(() => {
+    document.addEventListener('mouseup', onMouseUpBar);
+    document.addEventListener('mousemove', onMouseMoveBar);
+    return () => {
+      document.removeEventListener('mouseup', onMouseUpBar);
+      document.removeEventListener('mousemove', onMouseMoveBar);
+    };
   }, []);
 
   return (
@@ -54,7 +96,7 @@ const Scroll: React.FunctionComponent<Props> = (props) => {
         <div className="emily-scroll-bar" 
           // style={{ height: barHeight }}
           style={{height: barHeight, transform: `translateY(${barTop}px)`}}
-          // onMouseDown={onMouseDownBar}
+          onMouseDown={onMouseDownBar}
         />
       </div>
     </div>
